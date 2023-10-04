ef985c6d207a20301fd07d841909a24b77891616
erl-j <nicolasjonason@gmail.com>
2023-10-04 12:01:41 +0200

    basic directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lazy_drums',
+      version='0.1',
+      description='Minimal playback of MIDI drum sequences',
+      url='http://github.com/erl-j/lazy-drums',
+      author='erl-j',
+      author_email='njona@kth.se',
+      license='MIT',
+      packages=['lazy_drums'],
+      zip_safe=False)
